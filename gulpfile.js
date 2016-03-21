9f9a3c00c6f297fceb1040b8043cc6e2adc7c850
Bogdan Derevyaga <bogdan.derevyaga@gmail.com>
2016-03-21 15:48:09 +0200

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,50 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
     livereload = require('gulp-livereload'),
-	autoprefixer = require('gulp-autoprefixer'),
-	connect = require('gulp-connect'),
-	notify = require("gulp-notify");
+    autoprefixer = require('gulp-autoprefixer'),
+    connect = require('gulp-connect'),
+    wiredep = require('wiredep').stream,
+    notify = require("gulp-notify");
 
-gulp.task('default', ['connect', 'watch']);	
-	
-gulp.task('connect', function() {
-  connect.server({
-    root: '',
-    livereload: true
-  });
+gulp.task('default', ['connect', 'watch']);
+
+gulp.task('connect', function () {
+    connect.server({
+        root: '',
+        livereload: true
+    });
+});
+
+gulp.task('watch', function () {
+    gulp.watch('less/*.less', ['less']);
+    gulp.watch('*.html', ['html']);
+    gulp.watch('bower.json', ['bower']);
 });
 
 gulp.task('html', function () {
-  gulp.src('*.html')
-    .pipe(connect.reload());
+    gulp.src('*.html')
+        .pipe(connect.reload());
 });
-	
-gulp.task('less', function() {
-  gulp.src('less/*.less')
-    .pipe(less())
-	.pipe(autoprefixer({
+
+gulp.task('less', function () {
+    gulp.src('less/*.less')
+        .pipe(less())
+        .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-    .pipe(gulp.dest('css'))
-	.pipe(notify("CSS"))
-	.pipe(connect.reload());
+        .pipe(gulp.dest('css'))
+        .pipe(connect.reload());
 });
- 
-gulp.task('watch', function() {
-  gulp.watch('less/*.less', ['less']);
-  gulp.watch('*.html', ['html']);
+
+gulp.task('bower', function () {
+    gulp.src('index.html')
+        .pipe(wiredep({
+            directory: 'bower_components',
+            optional: 'configuration',
+            goes: 'here'
+        }))
+        .pipe(gulp.dest(''))
+        .pipe(connect.reload());
 });
 
