75f602446acc3d8a8c2b04d7c7ebca7fd6de5401
Bogdan Derevyaga <bogdan.derevyaga@gmail.com>
2016-03-26 14:57:48 +0200

    add rout

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
+var app = angular.module('myApp', ['ngRoute']);
 
+app.controller('MainController', function ($scope, $http) {
+    $http.get('https://api.github.com/users', {}).success(function (response) {
+        $scope.responses = response;
+    });
+});
+
+app.controller('UsersController', function ($scope, $routeParams, $http){
+    $scope.name = "UsersController";
+    $scope.userId = $routeParams.userId;
+    $http.get('https://api.github.com/users/' + $scope.userId , {}).success(function (response) {
+        $scope.userData = response;
+        $http.get(response.repos_url, {}).success(function (response) {
+            $scope.reposesData = response;
+        });
+    });
+});
+
+app.controller('ReposController', function ($scope, $routeParams, $http){
+    $scope.name = "ReposController";
+});
+
+app.config(function ($routeProvider, $locationProvider) {
+    const route = $routeProvider;
+    route.when('/',{
+        templateUrl: 'partials/main.html',
+        controller: 'MainController'
+    });
+    route.when('/user/:userId',{
+        templateUrl: 'partials/user.html',
+        controller: 'UsersController'
+    });
+    route.when('/user/:userId/:reposId',{
+        templateUrl: 'partials/repos.html',
+        controller: 'ReposController'
+    });
+});
